780b6f01bc4dfad65e874430f2d678aff8bf1c83
Dmitrij Korolewski <MaDaRaisBack@gmail.com>
2020-10-08 16:17:46 +0300

    init: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+	let stringNum = n.toString();
+
+	if(stringNum.includes('-')) {
+		return stringNum.substr(1).split('').reverse().join('');
+	} else {
+			return stringNum.split('').reverse().join('');
+    }
 }
